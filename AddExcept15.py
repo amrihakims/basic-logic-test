7d96b7bc0b28241ddfe2f1b7928cad37c404255e
Amri Hakim Sihite, ST <73640086+amrihakims@users.noreply.github.com>
2022-04-22 13:07:51 +0700

    Create AddExcept15.py
    New File for Test Basic Logic at BMG

---
+++ AddExcept15.py
@@ -0,0 +1,16 @@
+print('=====================')
+N = input('Rule : (N)  ')
+jumlah = 0
+Cetak = ""
+
+for i in range(1, int(N)+1):
+    if (i == 1):
+        Cetak = Cetak + str(i)
+    elif (i % 15 == 0):
+        Cetak = Cetak + ' + ' + 'BMG'
+        jumlah = jumlah
+    else:
+        Cetak = Cetak + ' + ' + str(i)
+        jumlah = jumlah + i
+
+print('Result : \n', Cetak, " = ", jumlah)
